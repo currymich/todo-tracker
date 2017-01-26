7a411ba8cb38663e7530e69b366bdbb0c891f42e
currymich <currymich@gmail.com>
2017-01-25 22:42:12 -0800

    wired up routes to views

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user.js')
 
-//SIGNUP ROUTE
+//SIGNUP ROUTE - show a signup page
+router.get('/signup', function(req, res){
+  res.render('users/signup.hbs');
+})
 
-//EDIT ROUTE
+//EDIT ROUTE - edit user details (only access your own or access any as admin)
+router.get('/:userId/edit', function(req, res){
+  res.render('users/edit.hbs');
+})
 
-//SHOW ROUTE
+//SHOW ROUTE - view user details (only access your own or access any as admin)
+router.get('/:userId', function(req, res){
+  res.render('users/show.hbs');
+})
 
-//CREATE ROUTE
+//CREATE ROUTE - receives data from signup form, creates new user
+router.post('/', function(req, res){
+  res.send('created something!')
+})
 
-//DELETE ROUTE
 
 module.exports = router;
